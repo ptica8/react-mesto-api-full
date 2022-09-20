f5a18479274c774949c2330625f30a637e5bcda5
Valeria Mikshina <lera-mikshina@mail.ru>
2022-09-20 15:36:43 +0300

    pr15

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -105,7 +105,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign(
-        { _id: user._id }, 'my-secret-key',
+        { _id: user._id }, process.env.JWT_SECRET || 'my-secret-key',
         { expiresIn: '7d' },
       );
       res.send({ token });
